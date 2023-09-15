acb735f990c822efab248bd4b12e567328de784a
Hulusi Şimşek <75800359+hulusimsek@users.noreply.github.com>
2023-09-15 11:59:46 +0300

    Update task3-q1 fixed.sql

--- task3-q1.sql
+++ task3-q1.sql
@@ -1,6 +1,9 @@
-WITH TASK3_Q1 AS(
-SELECT "DEPARTMENT_ID", MAX(SALARY)
-FROM HR."EMPLOYEES"
-GROUP BY "DEPARTMENT_ID" )
+WITH TASK3_Q1 AS (
+	SELECT department_id, MAX(salary) AS max_salary
+    FROM employees
+    GROUP BY department_id
+)
 
-SELECT * FROM TASK3_Q1;
+SELECT *
+FROM employees e
+JOIN TASK3_Q1 a ON e.DEPARTMENT_ID  = a.DEPARTMENT_ID AND e.salary = a.max_salary;
