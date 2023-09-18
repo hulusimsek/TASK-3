0fa6451a1cb06b7bd47da445fd6d4894cc10c930
Hulusi Şimşek <75800359+hulusimsek@users.noreply.github.com>
2023-09-18 13:14:44 +0300

    Update task3-q1.sql
    using analytic function

--- task3-q1.sql
+++ task3-q1.sql
@@ -1,9 +1,10 @@
-WITH TASK3_Q1 AS (
-	SELECT department_id, MAX(salary) AS max_salary
-    FROM employees
-    GROUP BY department_id
+WITH RankedEmployees AS (
+    SELECT
+        e.*,
+        RANK() OVER (PARTITION BY e.department_id ORDER BY e.salary DESC) AS salary_rank
+    FROM employees e
 )
 
 SELECT *
-FROM employees e
-JOIN TASK3_Q1 a ON e.DEPARTMENT_ID  = a.DEPARTMENT_ID AND e.salary = a.max_salary;
+FROM RankedEmployees
+WHERE salary_rank = 1
